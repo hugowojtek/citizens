a9d75bc3af30a69de15aa0ee8f81fdd4e1a1bca1
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-01-28 12:09:52 +0100

    zwierzęta

--- src/main/java/pl/sdacademy/citizens/CitizensApplication.java
+++ src/main/java/pl/sdacademy/citizens/CitizensApplication.java
@@ -13,39 +13,21 @@ import java.util.Map;
 public class CitizensApplication {
 
     private PersonReader personReader;
-    private AnimalReader animalReader;
     private PersonWriter personWriter;
 
 
     public CitizensApplication() {
         this.personReader = new PersonReader();
-        this.animalReader = new AnimalReader();
         this.personWriter = new PersonWriter();
     }
 
     public List<Person> process() throws ParseException {
         File personFile = new File(getClass().getClassLoader().getResource("person.csv").getFile());
         List<Person> people = personReader.readFromFile(personFile);
-        List<Animal> animals = processAnimals();
         System.out.println("Starting process of adding animals to people");
         int i = 0;
-        for (Animal animal : animals) {
-            while (i < people.size()) {
-                if (animal.getId().equals(people.get(i).getId())) {
-                    people.get(i).addAnimal(animal);
-                    break;
-                } else {
-                    i++;
-                }
-            }
-        }
         System.out.println("Done");
         return people;
-    }
-
-    private List<Animal> processAnimals() {
-        File animalFile = new File(getClass().getClassLoader().getResource("animal.csv").getFile());
-        return animalReader.readFromFile(animalFile);
     }
 
     public void writeModifiedEntries(List<Person> people) {
