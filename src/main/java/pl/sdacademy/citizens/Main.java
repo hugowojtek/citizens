eb2ef81d57ebf025dfc66fbc2b2870f52c98dafe
wozog <hugowojtek@o2.pl>
2018-02-06 14:15:12 +0100

    ver11

--- src/main/java/pl/sdacademy/citizens/Main.java
+++ src/main/java/pl/sdacademy/citizens/Main.java
@@ -4,7 +4,9 @@ import pl.sdacademy.citizens.model.Animal;
 import pl.sdacademy.citizens.model.Person;
 
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -13,7 +15,18 @@ public class Main {
         List<Person> people = app.process();
         List<Animal> animals = app.processAnimals();
 
-        Map<String,Integer> map1 = new MyCitizensApps().calculate(people);
+
+
+        List<Person> list50 = selectByName(people,"Marcin");
+
+        List<Person> list51 = selectedByName2(people,"Marcin");
+
+        Map<String,Integer> map0 = new MyCitizensApps().calculate(people);
+
+        Map<String,Integer> map1 = new MyCitizensApps().calculate_1(people);
+
+        Map<String,Long> map0_1 = countingByLastName(people);
+
 
         Map<String,List<String>> map2 = new MyCitizensApps().calculate_2(people);
 
@@ -21,9 +34,25 @@ public class Main {
 
         Map<String,List<Person>> map4 = new MyCitizensApps().calculate_4(people);
 
+        Map<String, List<Person>> map4_01 = nameGrouping(people);
+
+        Map<String,List<Person>> map4_02 = nameGroupingFiltered(people,"A");
+
+        Map<String, List<Person>> map50 = new MyCitizensApps().calculate_50(people);
+
+        Map<String, List<Person>> map50_1 = sexGrouping(people);
+
+        List<Person> list60 = selectedByAges(people);
+
+        List<Person> list70 = selectedRentiredPerson(people);
+
+        long counter = CountRentiredPeople(people);
+
         List<Person> list1 = new MyCitizensApps().calculate_5(people);
 
-        List<Person> list2 = new MyCitizensApps().calculate_6(people);
+        List<Person> list2_1 = new MyCitizensApps().calculate_6(people);
+
+        List<Person> list2_2 = selectedRentiredPerson(people);
 
         List<Person> list3 = new MyCitizensApps().calculate_7(people);
 
@@ -31,8 +60,21 @@ public class Main {
 
         Map<String,Integer> map5 = new MyCitizensApps().calculate_9(animals);
 
-        Map<String,List<Person>> map6 = new MyCitizensApps().calculate_10(people,animals);
+        List<Person> list5 = new MyCitizensApps().calculate_10(list4);
 
+        List<Person> list6_1 = new MyCitizensApps().calculate_11(people);
+
+        List<Person> list6_2 = filterLambda(people);
+
+        List<Person> list7 = new MyCitizensApps().calculate_12(list4);
+
+        Map<Integer,List<Person>> map6 = new MyCitizensApps().calculate_13(people);
+
+        Map<String,Integer> map7 = new MyCitizensApps().calculate_14(list4);
+
+        Long id_person=15L;
+        String species="CAT";
+        Integer cats = new MyCitizensApps().calculate_15(people,id_person,species);
 
         System.out.println();
 //        app.mapLastName(people);
@@ -41,5 +83,114 @@ public class Main {
 //        app.writeModifiedEntries(people);
 //        app.write1(people);
      }
+
+    private static List<Person> filterLambda(List<Person> people) {
+        long start = System.currentTimeMillis();
+        List<Person> list = people.stream().filter(person -> filterValidate(person)).collect(Collectors.toList());
+        long stop = System.currentTimeMillis();
+        System.out.println("lambda filtrowanie: "+(stop-start));
+        return list;
+    }
+
+    private static boolean filterValidate(Person person) {
+        if ((person.getName()==null)||((person.getName().length()<2))) return false;
+        if ((person.getLastName()==null)||((person.getLastName().length()<2))) return false;
+        if ((person.getSex().length()!=1)) return false;
+        if ((person.getBirthDate().isAfter(LocalDate.now()))) return false;
+        if (((LocalDate.now().getYear() - person.getBirthDate().getYear()) < 18)) return false;
+        if (((LocalDate.now().getYear() - person.getBirthDate().getYear()==18)&&
+            (!(LocalDate.now().getDayOfYear() > person.getBirthDate().getDayOfYear())))) return false;
+        return true;
+    }
+
+
+    private static List<Person> selectedRentiredPerson(List<Person> people) {
+        long start = System.currentTimeMillis();
+        List<Person> list = people.stream()
+                .filter(person -> person.getAge()>=60&&person.getSex().equals("F")||person.getAge()>=65&&person.getSex().equals("M"))
+                .collect(Collectors.toList());
+        long stop = System.currentTimeMillis();
+        System.out.println("lambda emerytowanych czas: "+(stop-start) );
+        return list;
+    }
+
+    private static long CountRentiredPeople(List<Person> people) {
+        long counter = 0;
+        for (Person person : people) {
+            if ((person.getSex().equals("F") && (person.getAge() >= 60)) || (person.getSex().equals("M")) && (person.getAge() >= 65))
+                counter++;
+        }
+        return counter;
+    }
+
+
+    private static Map<String, List<Person>> nameGrouping(List<Person> people) {
+        Map<String,List<Person>> map = people.stream().collect(Collectors.groupingBy(Person::getName));
+        return map;
+    }
+
+    private static Map<String, List<Person>> nameGroupingFiltered(List<Person> people, String str) {
+        Map<String,List<Person>> map = people.stream()
+                .filter(person -> person.getName().startsWith(str))
+                .collect(Collectors.groupingBy(Person::getName));
+        return map;
+    }
+
+
+
+    private static Map<String, List<Person>> sexGrouping(List<Person> people) {
+        Map<String,List<Person>> map = people.stream()
+                .filter(person -> validateSex(person))
+                .collect(Collectors.groupingBy(Person::getSex));
+        return map;
+    }
+
+
+
+
+    private static boolean validateSex(Person person){
+        if (person.getSex().equals("M")||(person.getSex().equals("F"))) return true;
+        return false;
+    }
+
+
+    private static Map<String,Long> countingByLastName(List<Person> people) {
+        Long start = System.currentTimeMillis();
+        Map<String,Long> map = people.stream()
+                .map(Person::getLastName)
+                .collect(Collectors.groupingBy(person -> person, Collectors.counting()));
+        Long stop = System.currentTimeMillis();
+        System.out.println("Policzone metoda_3 w "+ (stop - start));
+        return map;
+    }
+
+
+    private static List<Person> selectedByName2(List<Person> people, String word) {
+        long start = System.currentTimeMillis();
+        List<Person> list = new ArrayList();
+        for (Person person:people) {
+            if (person.getName().equals(word)) list.add(person);
+        }
+        long stop = System.currentTimeMillis();
+        System.out.println("metoda2"+"\t"+(stop-start));
+        return list;
+    }
+
+    private static List<Person> selectByName(List<Person> people, String word) {
+        long start = System.currentTimeMillis();
+        List<Person> selectedPerson = people.stream()
+                .filter(person -> person.getName().equals(word))
+                .collect(Collectors.toList());
+        long stop= System.currentTimeMillis();
+        System.out.println("metoda1"+"\t"+(stop-start));
+        return selectedPerson;
+    }
+
+    private static List<Person> selectedByAges(List<Person> people){
+        List<Person> list = people.stream()
+                .filter(person -> person.getAge()>35&&person.getAge()<55)
+                .collect(Collectors.toList());
+        return list;
+    }
 }
 //
--- src/main/java/pl/sdacademy/citizens/MyCitizensApps.java
+++ src/main/java/pl/sdacademy/citizens/MyCitizensApps.java
@@ -1,22 +1,28 @@
 package pl.sdacademy.citizens;
 
 
+import org.apache.commons.collections.map.LinkedMap;
 import pl.sdacademy.citizens.model.Animal;
 import pl.sdacademy.citizens.model.Person;
 
+import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class MyCitizensApps {
 
     //nazwiska i ilość wystąpień
 
     public Map<String, Integer> calculate(List<Person> people) {
-        Map<String, Integer> map = new LinkedHashMap<>();
+        long start = System.currentTimeMillis();
+        Map<String, Integer> map = new HashMap<>();
         for (Person person : people) {
             if (!(map.containsKey(person.getLastName())))
                 map.put(person.getLastName(), count(people, person.getLastName()));
 
         }
+        long stop = System.currentTimeMillis();
+        System.out.println("Policzone metoda_1 w "+ (stop - start));
         return map;
     }
 
@@ -29,8 +35,27 @@ public class MyCitizensApps {
         return counter;
     }
 
-    //nazwiska i lista imion
+    //nazwiska i ilość wystąpień inna metoda
+    public Map<String,Integer> calculate_1(List<Person> people) {
+        long start = System.currentTimeMillis();
+        Map<String,Integer> map = new HashMap<>();
+        for (Person person:people){
+            int val = map.getOrDefault(person.getLastName(),0);
+            val++;
+            map.put(person.getLastName(),val);
+        }
+        long stop = System.currentTimeMillis();
+        System.out.println("Policzone metoda_2 w "+ (stop - start));
+        return map;
+    }
 
+    //nazwiska i ilość imion streamy
+    public Map<String,Integer> calculate_1s(List<Person> people) {
+        //Map<String,Integer> collect = people.stream().collect(Collectors.groupingBy(Person::getLastName,Collectors.toList()))
+        return null;
+    }
+
+    //nazwiska i lista imion
     public Map<String, List<String>> calculate_2(List<Person> people) {
         Map<String, List<String>> map = new LinkedHashMap<>();
         for (Person person : people) {
@@ -39,6 +64,9 @@ public class MyCitizensApps {
         }
         return map;
     }
+
+
+
 
     private List<String> count_2(List<Person> people, String currentlastName) {
         List<String> list = new LinkedList<>();
@@ -100,30 +128,33 @@ public class MyCitizensApps {
     }
 
     public List<Person> calculate_6(List<Person> people) {
+        long start = System.currentTimeMillis();
         List<Person> list = new ArrayList<>();
         for (Person person : people) {
             if ((person.getAge() >= 60) && (person.getSex().equals("F"))) list.add(person);
             if ((person.getAge() >= 65) && (person.getSex().equals("M"))) list.add(person);
         }
+        long stop = System.currentTimeMillis();
+        System.out.println("foreach emerytowanych czas: "+(stop-start) );
         return list;
     }
     //name of person is neither empty nor shorter than 2 characters etc
 
-    public List<Person> calculate_7(List<Person> people){
+    public List<Person> calculate_7(List<Person> people) {
         List<Person> list = new ArrayList<>();
-        for (Person person:people){
+        for (Person person : people) {
             if (person.validateData()) list.add(person);
         }
-        return  list;
+        return list;
     }
 
     //add list of animals to person
 
     public List<Person> calculate_8(List<Person> people, List<Animal> animals) {
         List<Person> list = new ArrayList<>();
-        for (Person person:people){
-            for (Animal animal:animals){
-                if (person.getId().equals(animal.getId())){
+        for (Person person : people) {
+            for (Animal animal : animals) {
+                if (person.getId().equals(animal.getId())) {
                     person.addAnimal(animal);
 
                 }
@@ -134,13 +165,13 @@ public class MyCitizensApps {
         return list;
     }
 
-    //count how much animals every species
+    //count how many animals every species
 
-    public Map<String,Integer> calculate_9(List<Animal> animals){
-        Map<String,Integer> map = new LinkedHashMap<String,Integer>();
-        for (Animal animal:animals){
-            if (!(map.containsKey(animal.getSpecies()))){
-                map.put(animal.getSpecies(),count_9(animals,animal.getSpecies()));
+    public Map<String, Integer> calculate_9(List<Animal> animals) {
+        Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+        for (Animal animal : animals) {
+            if (!(map.containsKey(animal.getSpecies()))) {
+                map.put(animal.getSpecies(), count_9(animals, animal.getSpecies()));
             }
         }
         return map;
@@ -149,8 +180,8 @@ public class MyCitizensApps {
 
 
     private Integer count_9(List<Animal> animals, String species) {
-        Integer counter=0;
-        for (Animal animal:animals){
+        Integer counter = 0;
+        for (Animal animal : animals) {
             if (animal.getSpecies().equals(species)) {
                 counter++;
             }
@@ -158,36 +189,155 @@ public class MyCitizensApps {
         return counter;
     }
 
-    //check who person has an animals
+    //count how many people have at least ANIMAL_LIMITS
 
-    public Map<String,List<Person>> calculate_10(List<Person> people, List<Animal> animals) {
-        Map<String,List<Person>> map = new LinkedHashMap<>();
-        for (Animal animal:animals){
-            if (!(map.containsKey(animal.getSpecies()))){
-
-                for (Person person:people){
-                    if (animal.getId().equals(person.getId())){
-                        map.put(animal.getSpecies(),count_10(people,person.getId()));
-                    }
-                }
-            }
-        }
-        return map;
-    }
-
-    private List<Person> count_10(List<Person> people, Long id) {
-        List<Person> list = new LinkedList<>();
-        for (Person person:people){
-            if (person.getId().equals(id)) list.add(person);
+    public List<Person> calculate_10(List<Person> people) {
+        final int ANIMAL_LIMITS = 2;
+        List<Person> list = new ArrayList<>();
+        for (Person person : people) {
+            if ((person.getAnimalList() != null) && (person.getAnimalList().size() >= ANIMAL_LIMITS)) list.add(person);
         }
         return list;
     }
 
+    //filter of people
+
+    public List<Person> calculate_11(List<Person> people) {
+        long start = System.currentTimeMillis();
+        List<Person> list = new ArrayList<>();
+        for (Person person : people) {
+            if ((person.getName()==null)||(!(person.getName().length()>=2))) continue;
+            if ((person.getLastName()==null)||(!(person.getLastName().length()>=2))) continue;
+            if (!(person.getSex().length()==1)) continue;
+            if ((person.getBirthDate().isAfter(LocalDate.now()))) continue;
+            if (!((LocalDate.now().getYear() - person.getBirthDate().getYear()) >= 18)) continue;
+            if (((LocalDate.now().getYear() - person.getBirthDate().getYear()==18)&&
+                    (!(LocalDate.now().getDayOfYear() > person.getBirthDate().getDayOfYear())))) continue;
+
+            list.add(person);
+        }
+        long stop = System.currentTimeMillis();
+        System.out.println("foreach filtrowanie: "+(stop-start));
+        return list;
+    }
+
+    //rename of Last name asterix codes
+
+    public List<Person> calculate_12(List<Person> people) {
+        List<Person> list = new ArrayList<>();
+        for (Person person : people) {
+
+            if (person.getLastName().length() < 2) continue;
+            char[] table = person.getLastName().toCharArray();
+            char startLetter = table[0];
+            char endLetter = table[table.length - 1];
+            String strEndLetter = String.valueOf(endLetter).toLowerCase();
+            String asterix = "*****";
+            String result = startLetter + asterix + strEndLetter;
+            person.setLastName(result);
+            list.add(person);
+
+        }
+        return list;
+
+    }
+
+    //grouping of people
+
+    public Map<Integer, List<Person>> calculate_13(List<Person> people) {
+        Map<Integer, List<Person>> map = new LinkedHashMap<>();
+        List<Person> list25 = new ArrayList<>();
+        List<Person> list35 = new ArrayList<>();
+        List<Person> list45 = new ArrayList<>();
+        List<Person> listDown = new ArrayList<>();
+        List<Person> listUp = new ArrayList<>();
+
+        for (Person person : people) {
+
+            if (((LocalDate.now().getYear() - person.getBirthDate().getYear()) <= 20)) {
+                listDown.add(person);
+                continue;
+            }
+
+            if (((LocalDate.now().getYear() - person.getBirthDate().getYear()) > 20) && ((LocalDate.now().getYear() - person.getBirthDate().getYear()) <= 30)) {
+                list25.add(person);
+                continue;
+            }
+            if (((LocalDate.now().getYear() - person.getBirthDate().getYear()) > 30) && ((LocalDate.now().getYear() - person.getBirthDate().getYear()) <= 40)) {
+                list35.add(person);
+                continue;
+            }
+            if (((LocalDate.now().getYear() - person.getBirthDate().getYear()) > 40) && ((LocalDate.now().getYear() - person.getBirthDate().getYear()) <= 50)) {
+                list45.add(person);
+                continue;
+            }
+            if ((LocalDate.now().getYear() - person.getBirthDate().getYear()) > 50) {
+                listUp.add(person);
+                continue;
+            }
+
+        }
+        map.put(20, listDown);
+        map.put(25, list25);
+        map.put(35, list35);
+        map.put(45, list45);
+        map.put(50, listUp);
+
+        return map;
+    }
+
+    //counting of animals from people(animalsList) object
+
+    public Map<String, Integer> calculate_14(List<Person> people) {
+        Map<String, Integer> map = new LinkedMap();
+        Integer cats = 0;
+        Integer parrots = 0;
+        Integer fishes = 0;
+        Integer dogs = 0;
+        for (Person person : people) {
+            if (person.getAnimalList() == null) continue;
+            for (Animal animal : person.getAnimalList()) {
+
+                if (animal.getSpecies().equals("CAT")) {
+                    cats++;
+                }
+                if (animal.getSpecies().equals("PARROT")) {
+                    parrots++;
+                }
+                if (animal.getSpecies().equals("FISH")) {
+                    fishes++;
+                }
+                if (animal.getSpecies().equals("DOG")) {
+                    dogs++;
+                }
+            }
+
+        }
+        map.put("CAT", cats);
+        map.put("PARROT", parrots);
+        map.put("FISH", fishes);
+        map.put("DOG", dogs);
+
+        return map;
+    }
+
+    public Integer calculate_15(List<Person> people,Long id_person,String animalSpecies) {
+        Integer id=0;
+           for (Person person:people){
+               if (person.getId().equals(id_person)){
+                   for (Animal animal:person.getAnimalList()){
+                       if (animal.getSpecies().equals(animalSpecies)) id++;
+                   }
+
+               }
+           }
+        return id;
+    }
 
 
-
-
-
+    public Map<String,List<Person>> calculate_50(List<Person> people) {
+        return null;
+    }
 }
 
 
