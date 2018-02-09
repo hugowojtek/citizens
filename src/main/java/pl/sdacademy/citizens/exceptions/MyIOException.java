a244a5127a4a5d4b509b4ce14465a6fa8dbde6b5
wozog <hugowojtek@o2.pl>
2018-02-09 10:45:22 +0100

    2

---
+++ src/main/java/pl/sdacademy/citizens/exceptions/MyIOException.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.citizens.exceptions;
+
+import java.io.IOException;
+
+public class MyIOException extends IOException{
+    public MyIOException(String s) {
+        super(s);
+    }
+}
