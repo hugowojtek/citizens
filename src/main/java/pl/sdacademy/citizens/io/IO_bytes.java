edb26c20249048f0014fe5e0946f80ed68d94474
wozog <hugowojtek@o2.pl>
2018-02-09 10:45:41 +0100

    3

---
+++ src/main/java/pl/sdacademy/citizens/io/IO_bytes.java
@@ -0,0 +1,36 @@
+package pl.sdacademy.citizens.io;
+
+import pl.sdacademy.citizens.exceptions.MyIOException;
+
+import java.io.*;
+
+public class IO_bytes {
+
+    public void realization(String s, String s1) throws IOException{
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        int c;
+        int i = 0;
+        int[] tab = new int[255];
+        try {
+            inputStream = new FileInputStream(s);
+            outputStream = new FileOutputStream(s1);
+
+            while ((c = inputStream.read()) != -1) {
+                tab[i++] = c;
+                outputStream.write(c);
+
+            }
+        } finally {
+
+            if (inputStream!= null)
+                inputStream.close();
+
+            if (outputStream != null)
+                outputStream.close();
+
+        }
+
+        System.out.println();
+    }
+}
